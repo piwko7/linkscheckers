0774c0d124a0e52af37739c889548e404855a338
Piwko <marcinpiwko7@gmail.com>
2021-08-17 22:05:18 +0200

    5555

--- neutrino/settings.py
+++ neutrino/settings.py
@@ -122,9 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['staticfiles']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = ['staticfiles_css']
+
+
 
 
 # Default primary key field type
