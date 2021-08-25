efa39edc051992de5239b9b6e3ce3e1751c58e77
Piwko <marcinpiwko7@gmail.com>
2021-08-25 20:24:00 +0200

    Alpha 1.1.3

--- link_checker/link_chekcer.py
+++ link_checker/link_chekcer.py
@@ -16,4 +16,4 @@ def check_link(project_name, project_urls):
         if single_url in urls_on_site:
             print('To chyba dziala')
 
-check_link('https://najlepszy-ranking.pl/', ['http://www.musc.pl', 'http://ww.google.pl', 'https://szafa.pl/porady/293-ranking-butow-do-biegania-postaw-na-wygode.html'])
+check_link('https://zazyjkultury.pl/jaki-sklad-powinna-miec-najlepsza-karma-dla-psa/', ['https://najlepszy-ranking.pl/', 'http://ww.google.pl', 'https://szafa.pl/porady/293-ranking-butow-do-biegania-postaw-na-wygode.html'])
